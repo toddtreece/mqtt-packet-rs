55bf767c61cd653ea361913eddbebda3c411d2e0
Todd Treece <toddtreece@gmail.com>
2019-11-16 17:15:21 -0500

    add error module

---
+++ src/error.rs
@@ -0,0 +1,53 @@
+use std::error::Error as StdError;
+use std::fmt;
+use std::io::Error as IoError;
+use std::num::TryFromIntError;
+use std::string::FromUtf8Error;
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Error {
+  ParseError,
+  GenerateError,
+}
+
+impl StdError for Error {
+  fn description(&self) -> &str {
+    match *self {
+      Error::ParseError => "Unable to parse type",
+      Error::GenerateError => "Unable to generate data",
+    }
+  }
+}
+
+impl fmt::Display for Error {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    match *self {
+      Error::ParseError => f.write_str("ParseError"),
+      Error::GenerateError => f.write_str("GenerateError"),
+    }
+  }
+}
+
+impl From<IoError> for Error {
+  fn from(e: IoError) -> Self {
+    match e {
+      _ => Error::ParseError,
+    }
+  }
+}
+
+impl From<FromUtf8Error> for Error {
+  fn from(e: FromUtf8Error) -> Self {
+    match e {
+      _ => Error::ParseError,
+    }
+  }
+}
+
+impl From<TryFromIntError> for Error {
+  fn from(e: TryFromIntError) -> Self {
+    match e {
+      _ => Error::ParseError,
+    }
+  }
+}
